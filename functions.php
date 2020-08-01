4996672ee40697dba9cd4c8b1a3b2cb38b64c277
Ade Indra Saputra <adeayobandung@gmail.com>
2020-08-01 11:00:58 +0700

    add function slider

--- functions.php
+++ functions.php
@@ -50,3 +50,41 @@ function add_classes_on_li($classes, $item, $args) {
     return $classes;
 }
 add_filter('nav_menu_css_class','add_classes_on_li',1,3);
+
+// new menu post
+function slide_post_register(){
+    $labels = array(
+        'name'               => __( 'Slider' ),
+        'singular_name'      => __( 'Slider' ),
+        'add_new'            => __( 'Add New' ),
+        'add_new_item'       => __( 'Add New' ),
+        'edit_item'          => __( 'Edit' ),
+        'new_item'           => __( 'New Slider' ),
+        'all_items'          => __( 'All Slider' ),
+        'view_item'          => __( 'View Slider' ),
+        'search_items'       => __( 'Search Slider' ),
+        'not_found'          => __( 'No Slider found' ),
+        'not_found_in_trash' => __( 'No Slider found in the Trash' ), 
+        'parent_item_colon'  => '',
+        'menu_name'          => 'Slider'
+    );
+    $args = array(
+        'labels'        => $labels,
+        'description'   => 'Slider',
+        'public'        => true,
+		'menu_icon'     => 'dashicons-admin-page',
+        'menu_position' => 20,
+        'supports'      => array('title','editor', 'thumbnail'),
+		'exclude_from_search' => true,
+        'has_archive'   => false,
+        'show_ui'       => true,
+		'rewrite'       => array(
+					'slug'		 	=> 'slider',
+					'with_front'	=> true
+				),
+		
+    );
+    register_post_type( 'slider', $args );
+	flush_rewrite_rules();
+}
+add_action( 'init', 'slide_post_register' );
