56c89c0a5a9af7c2a74e7177da95d55dd723f9cd
Ade Indra Saputra <adeayobandung@gmail.com>
2020-09-08 13:55:57 +0700

    footer rpi

--- footer.php
+++ footer.php
@@ -4,11 +4,11 @@
 <div class="container bottom_border">
 <div class="row">
 <div class=" col-sm-4 col-md col-sm-4  col-12 col">
-<h5 class="headin5_amrc col_white_amrc pt2"><img src="https://market-expertise.com/wp-content/uploads/2020/01/ME-Logo-new-new-logo-white.png" width="200px"></h5>
+<h5 class="headin5_amrc col_white_amrc pt2"><img src="<?php echo get_template_directory_uri(); ?>/images/logo-rpi.png" width="200px"></h5>
 <!--headin5_amrc-->
-<p class="mb10">Market Expertise is a proficient B2B enterprise, trusted to deliver quality research and value-added to services through its industry practice of more <br>than 8 years.</p>
-<p><i class="fa fa-phone"></i>  +91-895-600-8984 / 85  </p>
-<p><i class="fa fa fa-envelope"></i> info@mepune.com  </p>
+<p class="mb10">Rumah Produktif Indonesia lahir minggu kedua pandemi covid-19 melanda negeri kita tercinta.</p>
+<p><i class="fa fa-phone"></i>  +62 813-4240-4140  </p>
+<p><i class="fa fa fa-envelope"></i> info@rpi.or.id  </p>
 </div>
 
 
@@ -16,11 +16,11 @@
 <h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
 <!--headin5_amrc-->
 <ul class="footer_ul_amrc">
-<li><a href="#">Market Research</a></li>
-<li><a href="#">Survey Programming</a></li>
-<li><a href="#">Data Vault</a></li>
-<li><a href="#">Lead Generation</a></li>
-<li><a href="#">Digital Marketing Services</a></li>
+<li><a href="#">Sejaran</a></li>
+<li><a href="#">Visi Misi</a></li>
+<li><a href="#">Program Kerja</a></li>
+<li><a href="#">Pengurus</a></li>
+<li><a href="#">Message From Presiden</a></li>
 </ul>
 <!--footer_ul_amrc ends here-->
 </div>
@@ -50,12 +50,11 @@
 
 
 <div class=" col-sm-4 col-md  col-12 col">
-<h5 class="headin5_amrc col_white_amrc pt2">Visit</h5>
+<h5 class="headin5_amrc col_white_amrc pt2">Alamat Redaksi</h5>
 <!--headin5_amrc ends here-->
 
 <ul class="footer_ul_amrc">
-<li><a href="#">IN : Office No 302,The Chambers, Viman Nagar, Pune - 411014, Maharashtra, India</p></li>
-<li><a href="#">USA : 300 Cadman Plaza West, One Pierrepont Plaza, 12th Floor Brooklyn NY 11201 USA</p></li>
+<li><a href="#">Jalan Juragan Sinda IV No. 16 G Kukusan Beji Depok</p></li>
 </ul>
 <!--footer_ul2_amrc ends here-->
 </div>
@@ -66,14 +65,14 @@
 <div class="container">
 <ul class="foote_bottom_ul_amrc">
 <li><a href="#">Home</a></li>
-<li><a href="#">Services</a></li>
-<li><a href="#">Solution</a></li>
-<li><a href="#">Career</a></li>
+<li><a href="#">Opini</a></li>
+<li><a href="#">Berita</a></li>
+<li><a href="#">Events</a></li>
 <li><a href="#">About Us</a></li>
 <li><a href="#">Contact Us</a></li>
 </ul>
 <!--foote_bottom_ul_amrc ends here-->
-<p class="text-center">© 2020 Market Expertise. All Rights Reserved.</p>
+<p class="text-center">© 2020 RPI - Manager IT. All Rights Reserved.</p>
 
 <ul class="social_footer_ul">
 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
